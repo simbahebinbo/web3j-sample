e5231ef84a5db83eef364a616576e2c89edb72f4
penghuaping <penghuaping@huobi.com>
2020-06-27 12:06:35 +0800

    整理代码

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ web3 for java 样例程序 (基于web3j 5.0.0)
 - [AccountManager](https://github.com/ethjava/web3j-sample/blob/master/src/main/java/com/ethjava/AccountManager.java) 账号相关接口
 - [TransactionClient](https://github.com/ethjava/web3j-sample/blob/master/src/main/java/com/ethjava/TransactionClient.java) eth转账相关接口
 - [EthInfo](https://github.com/ethjava/web3j-sample/blob/master/src/main/java/com/ethjava/EthInfo.java) 连接节点的相关信息接口
+- [IBAN](https://github.com/ethjava/web3j-sample/blob/master/src/main/java/com/ethjava/IBAN.java) 根据官方规则生成iban及付款二维码
 
 - [TokenClient](https://github.com/ethjava/web3j-sample/blob/master/src/main/java/com/ethjava/TokenClient.java) token代币相关查询及转账
 - [ColdWallet](https://github.com/ethjava/web3j-sample/blob/master/src/main/java/com/ethjava/ColdWallet.java) 冷钱包创建、交易
@@ -22,7 +23,6 @@ web3 for java 样例程序 (基于web3j 5.0.0)
 - [Filter](https://github.com/ethjava/web3j-sample/blob/master/src/main/java/com/ethjava/Filter.java) 新块、新交易相关监听
 - [ContractEvent](https://github.com/ethjava/web3j-sample/blob/master/src/main/java/com/ethjava/ContractEvent.java) 执行合约相关log监听
 - [DecodeMessage](https://github.com/ethjava/web3j-sample/blob/master/src/main/java/com/ethjava/DecodeMessage.java) 加密后的交易数据解析
-- [IBAN](https://github.com/ethjava/web3j-sample/blob/master/src/main/java/com/ethjava/IBAN.java) 根据官方规则生成iban及付款二维码
 - [Calculate](https://github.com/ethjava/web3j-sample/blob/master/src/main/java/com/ethjava/Calculate.java) 在发布合约前计算合约地址，根据签名后的交易信息计算TxHash
 - [Solidity](https://github.com/ethjava/web3j-sample/tree/master/src/main/java/com/ethjava/sol) 合约类相关
 - [LinkToken Support](https://github.com/ethjava/web3j-sample/tree/LinkToken) 链克支持
--- src/main/java/com/github/lansheng228/IBAN.java
+++ src/main/java/com/github/lansheng228/IBAN.java
@@ -10,77 +10,91 @@ import static java.lang.Integer.parseInt;
 public class IBAN {
 
   /**
-   * 根据官方支持的IBAN规则生成二维码 目前支持的有imtoken kcash 参考url
-   * https://github.com/ethereum/web3.js/blob/develop/lib/web3/iban.js 可以防止地址错误（有两位校验和）
+   * 根据官方支持的IBAN规则生成二维码
    */
   public static void main(String[] args) {
-    getIBAN();
+    String address = "0xb4352408a1fAa75f49256D7E0665292d164F608c";
+    String iban = getIBAN(address);
+    log.info("地址  " + address + "  生成二维码 " + iban);
   }
 
-  public static void getIBAN() {
-    String address = "0xaaae432f77a74a33c5e5b47612dabef44a905de6".toLowerCase();
-    log.info(address);
-    address = address.substring(2);
+  public static String getIBAN(String encodeAddress) {
+    log.info("encodeAddress: " + encodeAddress);
+    String address = encodeAddress.toLowerCase().substring(2);
     BigInteger value = new BigInteger(address, 16);
     StringBuilder bban = new StringBuilder(value.toString(36).toUpperCase());
     while (bban.length() < 15 * 2) {
       bban.insert(0, '0');
     }
-    log.info("bban " + bban);
+    log.info("bban: " + bban);
     String iban = "XE00" + bban;
 
     iban = iban.substring(4) + iban.substring(0, 4);
     StringBuilder code = new StringBuilder();
     for (int i = 0; i < iban.length(); i++) {
       char chr = iban.charAt(i);
-      if (chr >= 'A' && chr <= 'Z') {
-        int temp = chr - 'A' + 10;
-        code.append(String.valueOf(temp));
+      int temp;
+      if ((chr >= 'A') && (chr <= 'Z')) {
+        temp = chr - 'A' + 10;
       } else {
-        code.append(String.valueOf((chr - '0')));
+        temp = chr - '0';
       }
+      code.append(temp);
     }
-    //		log.info(code);
+    log.info("code: " + code);
     String remainder = code.toString();
     String block;
     while (remainder.length() > 2) {
-      int endPoint = remainder.length() >= 9 ? 9 : remainder.length();
+      int endPoint = Math.min(remainder.length(), 9);
       block = remainder.substring(0, endPoint);
       remainder = parseInt(block, 10) % 97 + remainder.substring(block.length());
-      //			log.info(remainder);
     }
+    log.info("remainder: " + remainder);
 
     int checkNum = parseInt(remainder, 10) % 97;
     String checkDigit = ("0" + (98 - checkNum));
     checkDigit = checkDigit.substring(checkDigit.length() - 2);
-    //		log.info(checkDigit);
+    log.info("checkDigit: " + checkDigit);
     String IBAN = "XE" + checkDigit + bban;
     String qrCodeString = "iban:" + IBAN + "?token=ETH&amount=5";
     log.info("IBAN " + IBAN);
     log.info("验证 " + validateIBAN(IBAN));
     log.info("qrcode " + qrCodeString);
-    decodeQRString(qrCodeString);
+    String decodeAddress = decodeQRString(qrCodeString);
+
+    if (encodeAddress.equalsIgnoreCase(decodeAddress)) {
+      return IBAN;
+    } else {
+      log.warn("生成的二维码无法解码");
+      return null;
+    }
   }
 
   private static boolean validateIBAN(String iban) {
     int len = iban.length();
-    if (len < 4 || !iban.matches("[0-9A-Z]+")) return false;
+    if ((len < 4) || (!iban.matches("[0-9A-Z]+"))) {
+      return false;
+    }
 
     iban = iban.substring(4) + iban.substring(0, 4);
 
     StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < len; i++) sb.append(Character.digit(iban.charAt(i), 36));
+    for (int i = 0; i < len; i++) {
+      sb.append(Character.digit(iban.charAt(i), 36));
+    }
 
     BigInteger bigInt = new BigInteger(sb.toString());
+    int mod = bigInt.mod(BigInteger.valueOf(97)).intValue();
+    log.info("mod: " + mod);
 
-    return bigInt.mod(BigInteger.valueOf(97)).intValue() == 1;
+    return mod == 1;
   }
 
-  private static void decodeQRString(String result) {
+  private static String decodeQRString(String result) {
     int ibanEndpoint = result.indexOf("?");
     String iban = result.substring(5, ibanEndpoint < 0 ? result.length() : ibanEndpoint);
     String address = IBAN2Address(iban);
-    String query = result.substring(ibanEndpoint + 1, result.length());
+    String query = result.substring(ibanEndpoint + 1);
     String[] params = query.split("&");
     String token = null;
     String amount = null;
@@ -96,7 +110,9 @@ public class IBAN {
     log.info("decodeQRString");
     log.info("address " + address);
     log.info("token " + token);
-    log.info("amount " + address);
+    log.info("amount " + amount);
+
+    return address;
   }
 
   private static String IBAN2Address(String iban) {
