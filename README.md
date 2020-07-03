4809318fd2b1858cf0e6f341f5cc44a81319e693
penghuaping <penghuaping@huobi.com>
2020-07-03 12:56:14 +0800

    整理代码

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # web3j-sample
-web3 for java 样例程序 (基于web3j 5.0.0)   
-环境 idea maven  
+web3 for java 样例程序
+
+基于web3j 5.0.0
+
+环境 maven  3.6.3
+
+
 运行前提 需要有一个开启RPC或者IPC服务的以太坊节点
 
 *  [搭建以太坊私有链节点](https://lansheng228.github.io/posts/6d8a95bf/)
@@ -16,13 +21,15 @@ web3 for java 样例程序 (基于web3j 5.0.0)
 - [IBAN](https://github.com/lansheng228/web3j-sample/blob/master/src/main/java/com/github/lansheng228/IBAN.java) 根据官方规则生成iban及付款二维码
 - [Calculate](https://github.com/lansheng228/web3j-sample/blob/master/src/main/java/com/github/lansheng228/Calculate.java) 在发布合约前计算合约地址，根据签名后的交易信息计算TxHash
 - [DecodeMessage](https://github.com/lansheng228/web3j-sample/blob/master/src/main/java/com/github/lansheng228/DecodeMessage.java) 加密后的交易数据解析
+- [Filter](https://github.com/lansheng228/web3j-sample/blob/master/src/main/java/com/github/lansheng228/Filter.java) 新块、新交易相关监听
+
+
 
 - [TokenClient](https://github.com/lansheng228/web3j-sample/blob/master/src/main/java/com/github/lansheng228/TokenClient.java) token代币相关查询及转账
 - [ColdWallet](https://github.com/lansheng228/web3j-sample/blob/master/src/main/java/com/github/lansheng228/ColdWallet.java) 冷钱包创建、交易
 - [TokenBalanceTask](https://github.com/lansheng228/web3j-sample/blob/master/src/main/java/com/github/lansheng228/TokenBalanceTask.java) 批量token代币余额查询
 - [Security](https://github.com/lansheng228/web3j-sample/blob/master/src/main/java/com/github/lansheng228/Security.java) 公钥私钥相关接口
 - [EthMnemonic](https://github.com/lansheng228/web3j-sample/blob/master/src/main/java/com/github/lansheng228/EthMnemonic.java) 生成、导入助记词(需要比特币的jar包 可以查看pom.xml)
-- [Filter](https://github.com/lansheng228/web3j-sample/blob/master/src/main/java/com/github/lansheng228/Filter.java) 新块、新交易相关监听
 - [ContractEvent](https://github.com/lansheng228/web3j-sample/blob/master/src/main/java/com/github/lansheng228/ContractEvent.java) 执行合约相关log监听
 - [Solidity](https://github.com/lansheng228/web3j-sample/tree/master/src/main/java/com/github/lansheng228/sol) 合约类相关
 
--- src/main/java/com/github/lansheng228/Filter.java
+++ src/main/java/com/github/lansheng228/Filter.java
@@ -1,114 +1,220 @@
-//package com.github.lansheng228;
-//
-//import com.github.lansheng228.utils.Environment;
-//import org.reactivestreams.Subscription;
-//import org.web3j.protocol.Web3j;
-//import org.web3j.protocol.core.DefaultBlockParameter;
-//import org.web3j.protocol.http.HttpService;
-//
-//import java.math.BigInteger;
-//
-//
-///**
-// * filter相关 监听区块、交易 所有监听都在Web3jRx中
-// */
-//public class Filter {
-//
-//  private static Web3j web3j;
-//
-//  public static void main(String[] args) {
-//    web3j = Web3j.build(Environment.getService());
-//    /**
-//     * 新区块监听
-//     */
-//    newBlockFilter(web3j);
-//    /**
-//     * 新交易监听
-//     */
-//    newTransactionFilter(web3j);
-//    /**
-//     * 遍历旧区块、交易
-//     */
-//    replayFilter(web3j);
-//    /**
-//     * 从某一区块开始直到最新区块、交易
-//     */
-//    catchUpFilter(web3j);
-//
-//    /**
-//     * 取消监听
-//     */
-//    //subscription.unsubscribe();
-//  }
-//
-//  private static void newBlockFilter(Web3j web3j) {
-//    Subscription subscription = web3j.
-//        blockObservable(false).
-//        subscribe(block -> {
-//          log.info("new block come in");
-//          log.info("block number" + block.getBlock().getNumber());
-//        });
-//  }
-//
-//  private static void newTransactionFilter(Web3j web3j) {
-//    Subscription subscription = web3j.
-//        transactionObservable().
-//        subscribe(transaction -> {
-//          log.info("transaction come in");
-//          log.info("transaction txHash " + transaction.getHash());
-//        });
-//  }
-//
-//  private static void replayFilter(Web3j web3j) {
-//    BigInteger startBlock = BigInteger.valueOf(2000000);
-//    BigInteger endBlock = BigInteger.valueOf(2010000);
-//    /**
-//     * 遍历旧区块
-//     */
-//    Subscription subscription = web3j.
-//        replayBlocksObservable(
-//            DefaultBlockParameter.valueOf(startBlock),
-//            DefaultBlockParameter.valueOf(endBlock),
-//            false).
-//        subscribe(ethBlock -> {
-//          log.info("replay block");
-//          log.info(ethBlock.getBlock().getNumber());
-//        });
-//
-//    /**
-//     * 遍历旧交易
-//     */
-//    Subscription subscription1 = web3j.
-//        replayTransactionsObservable(
-//            DefaultBlockParameter.valueOf(startBlock),
-//            DefaultBlockParameter.valueOf(endBlock)).
-//        subscribe(transaction -> {
-//          log.info("replay transaction");
-//          log.info("txHash " + transaction.getHash());
-//        });
-//  }
-//
-//  private static void catchUpFilter(Web3j web3j) {
-//    BigInteger startBlock = BigInteger.valueOf(2000000);
-//
-//    /**
-//     * 遍历旧区块，监听新区块
-//     */
-//    Subscription subscription = web3j.catchUpToLatestAndSubscribeToNewBlocksObservable(
-//        DefaultBlockParameter.valueOf(startBlock), false)
-//        .subscribe(block -> {
-//          log.info("block");
-//          log.info(block.getBlock().getNumber());
-//        });
-//
-//    /**
-//     * 遍历旧交易，监听新交易
-//     */
-//    Subscription subscription2 = web3j.catchUpToLatestAndSubscribeToNewTransactionsObservable(
-//        DefaultBlockParameter.valueOf(startBlock))
-//        .subscribe(tx -> {
-//          log.info("transaction");
-//          log.info(tx.getHash());
-//        });
-//  }
-//}
+package com.github.lansheng228;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import com.github.lansheng228.utils.Environment;
+import io.reactivex.disposables.Disposable;
+import lombok.extern.slf4j.Slf4j;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.methods.response.EthBlock;
+import org.web3j.protocol.core.methods.response.Transaction;
+import org.web3j.utils.Convert;
+
+
+/**
+ * filter 相关
+ *
+ * 监听区块、交易
+ *
+ * 所有监听都在Web3jRx中
+ */
+@Slf4j
+public class Filter {
+
+  private static Web3j web3j;
+
+  public static void main(String[] args) {
+    web3j = Web3j.build(Environment.getService());
+
+    newBlockSimpleFilter(web3j);
+
+    newBlockFullFilter(web3j);
+
+    newTransactionFilter(web3j);
+
+    replayBlockSimpleFilter(web3j);
+
+    replayBlockFullFilter(web3j);
+
+    replayTransactionFilter(web3j);
+
+    catchUpBlockSimpleFilter(web3j);
+
+    catchUpBlockFullFilter(web3j);
+
+    catchUpTransactionFilter(web3j);
+
+    countingTransactionFilter(web3j);
+  }
+
+
+  /**
+   * 新区块监听  详细信息
+   */
+  private static void newBlockSimpleFilter(Web3j web3j) {
+    Disposable subscription = web3j.blockFlowable(false)
+        .subscribe(ethBlock -> log.info("new block come in   " + "  block number  " + ethBlock.getBlock().getNumber()));
+  }
+
+  /**
+   * 新区块监听  简略信息
+   */
+  private static void newBlockFullFilter(Web3j web3j) {
+
+    Disposable subscription = web3j.blockFlowable(true)
+        .subscribe(ethBlock -> {
+          EthBlock.Block block = ethBlock.getBlock();
+          LocalDateTime timestamp = Instant.ofEpochSecond(block.getTimestamp().longValueExact()).atZone(ZoneId.of("UTC")).toLocalDateTime();
+          int transactionCount = block.getTransactions().size();
+          String hash = block.getHash();
+          String parentHash = block.getParentHash();
+
+          log.info(timestamp + " " +
+              "Tx count: " + transactionCount + ", " +
+              "Hash: " + hash + ", " +
+              "Parent hash: " + parentHash);
+        });
+  }
+
+
+  /**
+   * 新交易监听
+   */
+  private static void newTransactionFilter(Web3j web3j) {
+    Disposable subscription = web3j.transactionFlowable()
+        .subscribe(transaction -> log.info("transaction come in   " + "  transaction txHash " + transaction.getHash()));
+  }
+
+  /**
+   * 遍历旧区块
+   *
+   * 区块简略信息
+   */
+  private static void replayBlockSimpleFilter(Web3j web3j) {
+    BigInteger startBlock = BigInteger.valueOf(1);
+    BigInteger endBlock = BigInteger.valueOf(5);
+
+    Disposable subscription = web3j.
+        replayPastBlocksFlowable(
+            DefaultBlockParameter.valueOf(startBlock),
+            DefaultBlockParameter.valueOf(endBlock),
+            false).
+        subscribe(ethBlock -> log.info("replay block   " + "  block number  " + ethBlock.getBlock().getNumber()));
+  }
+
+  /**
+   * 遍历旧区块
+   *
+   * 区块详细信息
+   */
+  private static void replayBlockFullFilter(Web3j web3j) {
+    BigInteger startBlock = BigInteger.valueOf(1);
+    BigInteger endBlock = BigInteger.valueOf(5);
+
+    Disposable subscription = web3j.
+        replayPastBlocksFlowable(
+            DefaultBlockParameter.valueOf(startBlock),
+            DefaultBlockParameter.valueOf(endBlock),
+            true).
+        subscribe(ethBlock -> {
+          EthBlock.Block block = ethBlock.getBlock();
+          LocalDateTime timestamp = Instant.ofEpochSecond(block.getTimestamp().longValueExact()).atZone(ZoneId.of("UTC")).toLocalDateTime();
+          int transactionCount = block.getTransactions().size();
+          String hash = block.getHash();
+          String parentHash = block.getParentHash();
+
+          log.info(timestamp + " " +
+              "Tx count: " + transactionCount + ", " +
+              "Hash: " + hash + ", " +
+              "Parent hash: " + parentHash);
+        });
+  }
+
+  /**
+   * 遍历旧交易
+   */
+  private static void replayTransactionFilter(Web3j web3j) {
+    BigInteger startBlock = BigInteger.valueOf(1);
+    BigInteger endBlock = BigInteger.valueOf(5);
+
+    Disposable subscription = web3j.replayPastTransactionsFlowable(
+        DefaultBlockParameter.valueOf(startBlock),
+        DefaultBlockParameter.valueOf(endBlock)).
+        subscribe(transaction -> log.info("replay transaction  " + "   txHash   " + transaction.getHash()));
+  }
+
+  /**
+   * 从某一区块开始直到最新区块
+   *
+   * 遍历旧区块，监听新区块
+   *
+   * 区块简略信息
+   */
+  private static void catchUpBlockSimpleFilter(Web3j web3j) {
+    BigInteger startBlock = BigInteger.valueOf(1);
+
+    Disposable subscription = web3j.replayPastAndFutureBlocksFlowable(
+        DefaultBlockParameter.valueOf(startBlock), false)
+        .subscribe(ethBlock -> log.info("block  " + "  block number  " + ethBlock.getBlock().getNumber()));
+  }
+
+  /**
+   * 从某一区块开始直到最新区块
+   *
+   * 遍历旧区块，监听新区块
+   *
+   * 区块详细信息
+   */
+  private static void catchUpBlockFullFilter(Web3j web3j) {
+    BigInteger startBlock = BigInteger.valueOf(1);
+
+    Disposable subscription = web3j.replayPastAndFutureBlocksFlowable(
+        DefaultBlockParameter.valueOf(startBlock), true)
+        .subscribe(ethBlock -> {
+          EthBlock.Block block = ethBlock.getBlock();
+          LocalDateTime timestamp = Instant.ofEpochSecond(block.getTimestamp().longValueExact()).atZone(ZoneId.of("UTC")).toLocalDateTime();
+          int transactionCount = block.getTransactions().size();
+          String hash = block.getHash();
+          String parentHash = block.getParentHash();
+
+          log.info(timestamp + " " +
+              "Tx count: " + transactionCount + ", " +
+              "Hash: " + hash + ", " +
+              "Parent hash: " + parentHash);
+        });
+  }
+
+  /**
+   * 从某一区块开始直到最新交易
+   *
+   * 遍历旧交易，监听新交易
+   */
+  private static void catchUpTransactionFilter(Web3j web3j) {
+    BigInteger startBlock = BigInteger.valueOf(1);
+
+    Disposable subscription = web3j.replayPastAndFutureTransactionsFlowable(
+        DefaultBlockParameter.valueOf(startBlock))
+        .subscribe(tx -> log.info("transaction" + "   txHash   " + tx.getHash()));
+  }
+
+  /**
+   * 监听新交易并统计交易数额
+   */
+  private static void countingTransactionFilter(Web3j web3j) {
+    Disposable subscription = web3j.transactionFlowable()
+        .take(3)
+        .map(Transaction::getValue)
+        .reduce(BigInteger.ZERO, BigInteger::add)
+        .subscribe(total -> {
+          BigDecimal value = new BigDecimal(total);
+          log.info("Transaction value: " + Convert.fromWei(value, Convert.Unit.ETHER) + " Ether (" + value + " Wei)");
+        });
+  }
+}
+
+
