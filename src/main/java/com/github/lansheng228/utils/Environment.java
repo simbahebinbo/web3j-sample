119021a4c5b3dad8ab35b145e32f5418aab56fa3
penghuaping <penghuaping@huobi.com>
2021-02-19 13:07:50 +0800

    支持节点用户名和密码登录

--- src/main/java/com/github/lansheng228/utils/Environment.java
+++ src/main/java/com/github/lansheng228/utils/Environment.java
@@ -20,12 +20,12 @@ public class Environment {
   public static HttpService getService() {
     HttpService httpService = new HttpService(CommonConstant.NODE_URL);
     log.info(
-        "连接 ETH 节点 {}  的用户名 {} 和密码 {}  用户名是否为空: {}  密码是否为空: {} ",
+        "连接 ETH 节点 {}  的 用户名 {} 和 密码 {}  用户名是否为空: {}  密码是否为空: {} ",
         CommonConstant.NODE_URL,
         CommonConstant.NODE_USER,
         CommonConstant.NODE_PASSWORD,
-        StringUtils.isNotEmpty(CommonConstant.NODE_USER),
-        StringUtils.isNotEmpty(CommonConstant.NODE_PASSWORD));
+        StringUtils.isEmpty(CommonConstant.NODE_USER),
+        StringUtils.isEmpty(CommonConstant.NODE_PASSWORD));
 
     if (StringUtils.isNotEmpty(CommonConstant.NODE_USER) && StringUtils.isNotEmpty(CommonConstant.NODE_PASSWORD)) {
       log.info("登录 ETH 节点 需要验证 添加报文头");
